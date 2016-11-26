153aec41c40deaad1f8f64da84996d4f7de967ea
fenlan <1475307818@qq.com>
2016-11-26 23:22:33 +0800

    search

--- search.cpp
+++ search.cpp
@@ -1,3 +1,7 @@
+/*Author:fenlan
+  function:Sequential Search and Binary Search
+  date:2016.11.26
+ */
 #include <stdio.h>
 #include <stdlib.h>
 typedef int ElemType;
@@ -8,9 +12,9 @@ typedef struct Data{
 
 bool GetData(DATA &data, char *FileName);
 //从FileName 中获取数据,将数据地址指针传给data
-bool Shun_Xu_search(DATA &data, ElemType, int &address);
-//在data 中顺序查找elem 元素，找到返回true ,并返回地址
-bool Di_Gui_Search(DATA &data, ElemType elem, int &address);
+bool Sequential_Search(DATA &data, ElemType, int &address);
+//在data 中顺序查找elem 元素，找到返回true ,并返回地址,提供查找区间
+bool Di_Gui_Search(DATA &data, ElemType elem, int &address, int low, int high);
 //递归方法将已经排序好的data 进行折半查找
 bool No_Di_Gui_Search(DATA &data, ElemType elem, int &address);
 //非递归方法将已经排序好的data 进行非排序查找
@@ -21,6 +25,7 @@ int main (){
     DATA data;
     int i = 0, address = 0;
     ElemType elem = 0;
+    int number = 0;
 
     if (GetData(data, "datafile.txt") == false)
         printf("fail to get the data!\n");
@@ -32,8 +37,9 @@ int main (){
         printf("%d ", data[i].elem);
 
     printf("\n");
+    printf("address = %d\n", address);                   //test
 
-    if (No_Di_Gui_Search(data, 64, address) == false)
+    if (Di_Gui_Search(data, 53, address, 0, data->length-1) == false)
         printf("fail to search the elem!\n");
     else
         printf("The address is %d\n", address);
@@ -57,7 +63,7 @@ bool GetData(DATA &data, char *FileName){
     }
 }
 
-bool Shun_Xu_search(DATA &data, ElemType elem, int &address){
+bool Sequential_Search(DATA &data, ElemType elem, int &address){
     int i = 0;
 
     for (i = 0; i < data->length; i++)
@@ -73,23 +79,40 @@ bool Shun_Xu_search(DATA &data, ElemType elem, int &address){
     else     return true;
 }
 
-bool Di_Gui_Search(DATA &data, ElemType elem, int &address){
+bool Di_Gui_Search(DATA &data, ElemType elem, int &address, int low, int high){
+    int middle = 0;
+    middle = (low + high) / 2;
 
+    printf("low = %d  high = %d\n", low, high);             //test
+
+    if (low > high)            return false;
+    else if (data[middle].elem == elem){
+        address = middle;
+        return true;
+    }
+    else if (data[middle].elem < elem){
+        if (Di_Gui_Search(data, elem, address, middle+1, high))      return true;
+        else       return false;
+    }
+    else{
+        if (Di_Gui_Search(data, elem, address, low, middle-1))       return true;
+        else       return false;
+    }
 }
 
 bool No_Di_Gui_Search(DATA &data, ElemType elem, int &address){
     int low = 0, high = data->length;
-    int now = 0;
+    int middle = 0;
 
-    now = (low + high) / 2;
-    for (; high >= low;){
-        if (elem == data[now].elem){
-            address = now;
+    middle = (low + high) / 2;
+    for (; low <= high;){
+        if (elem == data[middle].elem){
+            address = middle;
             break;
         }
-        else if (elem < data[now].elem)         high = now - 1;
-        else           low = now + 1;
-        now = (low + high) /2;
+        else if (elem < data[middle].elem)         high = middle - 1;
+        else           low = middle + 1;
+        middle = (low + high) /2;
     }
 
     if (high < low)        return false;
