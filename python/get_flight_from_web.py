47285044474436899a67dc18aaca2a743419213d
fenlan <1475307818@qq.com>
2017-05-06 22:41:26 +0800

    python

--- python/get_flight_from_web.py
+++ python/get_flight_from_web.py
@@ -3,13 +3,13 @@ import sys
 import urllib.request
 from bs4 import BeautifulSoup
 
-url = "http://flights.ctrip.com/schedule/sia.kwe.html"
+url = "http://flights.ctrip.com/schedule/sia.ckg.html?P+"
 html = urllib.request.urlopen(url).read()
 soup = BeautifulSoup(html,"lxml")
 td = soup.find("tbody",id="flt1")
 
 for i in td.find_all('tr'):
     #print(i.contents[1].contents[1].contents[1].contents[0])    #get flight_name
-    print(i.contents[3].contents[1].contents[0])                #get flight_source_time
+    #print(i.contents[3].contents[1].contents[0])                #get flight_source_time
     #print(i.contents[7].contents[1].contents[0])                #get flight_arr_time
-    #can not get the price beacuse someday the price is not given
+    print(i.contents[13].contents[1].contents[1])                #get price
