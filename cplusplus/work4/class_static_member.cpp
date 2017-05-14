76c29a61fb772b1fd1e6d30edf7138134c078271
fenlan <1475307818@qq.com>
2017-05-14 13:53:46 +0800

    c++

---
+++ cplusplus/work4/class_static_member.cpp
@@ -0,0 +1,56 @@
+/* --------------------------------------------------
+ * 	Author: fenlan
+ * 	Date: 2017.5.14
+ * 	Desc: I can not sure the program is true
+ * --------------------------------------------------
+ *  	Define two classes, each with a static member,
+ *  so that the construction of each static member
+ *  involves a reference to the other.
+ * --------------------------------------------------
+ */
+
+
+#include <iostream>
+using namespace std;
+
+class Obj1 {
+	//static int default_value;
+public:
+	static int& default_value;
+	Obj1(int value) {
+		default_value = value;    
+	}
+	void print() {
+		cout << default_value << endl;    
+	}
+};
+
+class Obj2 {
+	//static int default_value;
+public:
+	static int& default_value;
+	Obj2(int value) {
+		default_value = value;    
+	}
+	void print() {
+		cout << default_value << endl;    
+	}
+};
+
+int j = 0;
+int& Obj1::default_value = j;
+int& Obj2::default_value = Obj1::default_value;
+
+int main()
+{
+	Obj1 a(1);
+	Obj2 b(2);
+	Obj1 c(3);
+	Obj2 d(4);
+	a.print();
+	b.print();
+	c.print();
+	d.print();
+
+	return 0;
+}
---
+++ cplusplus/work4/obj_con_des.cpp
@@ -0,0 +1,46 @@
+/* -------------------------------------------
+ * Author: fenlan
+ * Date: 2017.5.13
+ * -------------------------------------------
+ *  Given this program:
+ *
+ *  	#include <iostream>
+ *
+ *  	int main()
+ *  	{
+ *  	    std::cout << "Hello world!\n";
+ *  	}
+ *
+ *  modify it to produce this output:
+ *
+ *  	Initialize
+ *  	Hello world!
+ *  	Clean up
+ * --------------------------------------------
+ */
+
+
+#include <iostream>
+
+// define a class with constructor and destructor
+class init {
+public:
+	init() {    
+		std::cout << "Initialize\n";    
+	}
+	~init() {
+		std::cout << "Clean up\n";    
+	}
+};
+
+init i;			//the init() member function is working
+
+int main()
+{
+	std::cout << "Hello world!\n";    
+}
+
+//After the main(), the scope of object i is over so
+//the ~init() member function is working
+
+//At last, the program is over
