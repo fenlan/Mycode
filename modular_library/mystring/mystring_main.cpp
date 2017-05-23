0945c5653ebd4eeef7b4dc7ef1312f904e612e0b
fenlan <1475307818@qq.com>
2017-05-23 14:11:10 +0800

    c++

---
+++ modular_library/mystring/mystring_main.cpp
@@ -0,0 +1,15 @@
+#include "mystring.hpp"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    String str = "abc";
+    String str1 = "edc";
+    String str2 = str;
+    str1 = str;
+    cout << str << "\n";
+    cout << str1 << "\n";
+    cout << str2<< "\n";
+    return 0;
+}
