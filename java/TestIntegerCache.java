db43907a5c5ba58d5924fde0bf0f24938e72fef4
fenlan <1475307818@qq.com>
2017-07-22 13:23:40 +0800

    java

--- java/TestIntegerCache.java
+++ java/TestIntegerCache.java
@@ -9,18 +9,20 @@
  * 	Integer 的作者在写这个类时，为了避免重复创建
  * 对象，对Integer 值做了缓存，如果这个值在缓存范围
  * 内，直接返回缓存好的对象，否则new一个新的对象返回。
- * (缓存范围:-128~127)
+ * (缓存范围:-128~127) java 中相等比较用 equals。
  * ------------------------------------------
  */
 
-public class TestIntegerCache {
-	public static void main(String[] args) {
-		Integer i3 = 100;
-		Integer i4 = 100;
-		System.out.println(i3 == i4);
+ public class TestIntegerCache {
+ 	public static void main(String[] args) {
+ 		Integer i3 = 100;
+ 		Integer i4 = 100;
+ 		System.out.println(i3 == i4);
+ 		System.out.println(i3.equals(i4));
 
-		Integer i5 = 1000;
-		Integer i6 = 1000;
-		System.out.println(i5 == i6);
-	}
-}
+ 		Integer i5 = 1000;
+ 		Integer i6 = 1000;
+ 		System.out.println(i5 == i6);
+ 		System.out.println(i5.equals(i6));
+ 	}
+ }
