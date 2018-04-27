78767e5a43826136499cdb7421ef5feb58b5f744
fenlan <1475307818@qq.com>
2018-04-27 08:38:10 +0800

    Longest Common SubString

--- IntroductionToAlgorithms/lcSubString.cpp
+++ IntroductionToAlgorithms/lcSubString.cpp
@@ -1,31 +1,47 @@
 #include <iostream>
 #include <string.h>
+#include <stdlib.h>
+ 
 using namespace std;
  
-int max(int a, int b)
-{   return (a > b)? a : b; }
- 
-int LCSubStr(char *X, char *Y, int m, int n)
+void printLCSubStr(char* X, char* Y, int m, int n)
 {
-    int LCSuff[m+1][n+1];
-    int result = 0;
- 
-    for (int i=0; i<=m; i++)
-    {
-        for (int j=0; j<=n; j++)
-        {
+    int LCSuff[m + 1][n + 1];
+    int len = 0;
+    int row, col;
+
+    for (int i = 0; i <= m; i++) {
+        for (int j = 0; j <= n; j++) {
             if (i == 0 || j == 0)
                 LCSuff[i][j] = 0;
  
-            else if (X[i-1] == Y[j-1])
-            {
-                LCSuff[i][j] = LCSuff[i-1][j-1] + 1;
-                result = max(result, LCSuff[i][j]);
-            }
-            else LCSuff[i][j] = 0;
+            else if (X[i - 1] == Y[j - 1]) {
+                LCSuff[i][j] = LCSuff[i - 1][j - 1] + 1;
+                if (len < LCSuff[i][j]) {
+                    len = LCSuff[i][j];
+                    row = i;
+                    col = j;
+                }
+            } else
+                LCSuff[i][j] = 0;
         }
     }
-    return result;
+ 
+    if (len == 0) {
+        cout << "No Common Substring";
+        return;
+    }
+ 
+    char* resultStr = (char*)malloc((len + 1) * sizeof(char));
+    resultStr[len] = ' ';
+
+    while (LCSuff[row][col] != 0) {
+        resultStr[--len] = X[row - 1];
+        row--;
+        col--;
+    }
+ 
+    cout << resultStr;
 }
  
 int main()
@@ -36,7 +52,6 @@ int main()
     int m = strlen(X);
     int n = strlen(Y);
  
-    cout << "Length of Longest Common Substring is "
-         << LCSubStr(X, Y, m, n);
+    printLCSubStr(X, Y, m, n);
     return 0;
 }
